7d6b022942acc5a9ff60ef0997b7d19022be00c6
emon1995 <emonhossain995@gmail.com>
2023-01-04 17:25:23 +0600

    Minute js complete

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // const hr = document.querySelector("#hr");
-// const mn = document.querySelector("#mn");
+const mn = document.querySelector("#mn");
 const sc = document.querySelector("#sc");
 
 const deg = 6;
@@ -7,7 +7,9 @@ const deg = 6;
 setInterval(() => {
   let day = new Date();
   let ms = day.getMilliseconds() * deg;
+  let mm = day.getMinutes() * deg;
   let ss = day.getSeconds() * deg + ms / 1000;
 
+  mn.style.transform = `rotateZ(${mm}deg)`;
   sc.style.transform = `rotateZ(${ss}deg)`;
 });
