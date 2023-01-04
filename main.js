22617c2ce3e93db5c0b06631d5070b1516e795d9
emon1995 <emonhossain995@gmail.com>
2023-01-04 17:27:41 +0600

    Hours js complete

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// const hr = document.querySelector("#hr");
+const hr = document.querySelector("#hr");
 const mn = document.querySelector("#mn");
 const sc = document.querySelector("#sc");
 
@@ -7,9 +7,11 @@ const deg = 6;
 setInterval(() => {
   let day = new Date();
   let ms = day.getMilliseconds() * deg;
+  let hh = day.getHours() * 30;
   let mm = day.getMinutes() * deg;
   let ss = day.getSeconds() * deg + ms / 1000;
 
+  hr.style.transform = `rotateZ(${hh}deg)`;
   mn.style.transform = `rotateZ(${mm}deg)`;
   sc.style.transform = `rotateZ(${ss}deg)`;
 });
